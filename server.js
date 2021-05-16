d85589c83118e06a4103036c78aa6aec90f427b2
harlita <harlitad@gmail.com>
2021-05-16 20:50:00 +0700

    add routers

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const {errorHandler, errorNotFound} = require("./src/middleware/errorHandler");
+const router = require("./src/router");
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(express.json())
 app.get("/", (req, res, next) => {
   res.json({
     success: true,
     message: "Movie - Cast Rest",
   });
 });
+
+app.use("/api/v1", router)
+
+app.use(errorHandler)
+app.use(errorNotFound)
 
 app.listen(port, () => console.log(`app run on port : ${port}`));
