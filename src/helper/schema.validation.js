9f9e83d4b61eebe2ca431370e0a146763dfc9666
harlita <harlitad@gmail.com>
2021-05-17 00:14:12 +0700

    add createmovie schema

--- src/helper/schema.validation.js
+++ src/helper/schema.validation.js
@@ -14,7 +14,16 @@ let updateCast = yup.object().shape({
   rating: yup.number().max(5).min(0),
 });
 
+let createMovie = yup.object().shape({
+  name: yup.string().required(),
+  language: yup.string().required(),
+  status: yup.string().required(),
+  rating: yup.number().required().max(5).min(0),
+  casts: yup.array().required().min(1)
+});
+
 module.exports = {
   createCast,
-  updateCast
+  updateCast,
+  createMovie
 };
